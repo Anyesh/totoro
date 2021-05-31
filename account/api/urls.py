bf1f1f3b94c88d3dd1590690664d2293d8b25775
Anyesh <sthacruz@gmail.com>
2021-05-31 10:54:02 +0545

    feat: Add hint for httponly cookie auth

--- account/api/urls.py
+++ account/api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from .views import editProfile, login, logout, searchUsers, signup, userInfo
+# from .views import get_csrf, RefreshToken, Ping
+from .views import (LoginView, editProfile, logout, searchUsers, signup,
+                    userInfo)
 
 urlpatterns = [
+    # path("api/user/get-csrf/", get_csrf),
     path("api/user/signup/", signup),
-    path("api/user/login/", login),
+    path("api/user/login/", LoginView),
+    # path("api/user/ping/", Ping.as_view()),
+    # path("api/user/refresh-token/", RefreshToken.as_view()),
     path("api/user/logout/", logout),
     path("api/user/edit/", editProfile),
     path("api/user/search/<query>/", searchUsers),
