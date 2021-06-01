4e08272fd71add5374ec094608adb2ac78b82e21
Anyesh <sthacruz@gmail.com>
2021-06-01 15:32:28 +0545

    fix: Update API routes

---
+++ accounts/api/urls.py
@@ -0,0 +1,14 @@
+# from .views import get_csrf, RefreshToken, Ping
+from django.urls import path
+
+from accounts.api.views import Ping, editProfile, get_user_info, searchUsers, signup
+
+urlpatterns = [
+    # path("api/user/get-csrf/", get_csrf),
+    path("user/signup/", signup),
+    path("user/ping/", Ping.as_view()),
+    # path("user/refresh-token/", RefreshToken.as_view()),
+    path("user/edit/", editProfile),
+    path("user/search/<query>/", searchUsers),
+    path("user/u/<slug>/", get_user_info),
+]
