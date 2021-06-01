20186a2c0d3c508d9d236c3bf656b860bbd87e27
Anyesh <sthacruz@gmail.com>
2021-06-01 15:31:18 +0545

    fix: Remove token model admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 
-from accounts.models import Gender, Profile, Token, User
+from accounts.models import Gender, Profile, User
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(Gender)
-
-admin.site.register(Token)
