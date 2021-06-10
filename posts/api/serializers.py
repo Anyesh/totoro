7fad36cfd91d6b12d0f03d6484bd77f63ad254e3
Anyesh <sthacruz@gmail.com>
2021-06-10 18:06:35 +0545

    chore: Update serializer with new model

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -2,35 +2,61 @@ from rest_framework import serializers
 
 from accounts.api.serializers import UserSerializer
 from accounts.models import User
-from posts.models import Comment, Posts
+from posts.models import RESIZE_THRESH, Comment, Posts
 
 
 class PostsSerializer(serializers.ModelSerializer):
 
     likes = serializers.SerializerMethodField()
+    src = serializers.SerializerMethodField()
+
+    def get_src(self, obj):
+        request = self.context.get("request")
+        return {
+            "original": {
+                "url": request.build_absolute_uri(obj.image.url),
+                "height": obj.height,
+                "width": obj.width,
+            },
+            "thumbnail": {
+                "url": request.build_absolute_uri(obj.thumbnail.url),
+                "height": int(obj.height * RESIZE_THRESH),
+                "width": int(obj.width * RESIZE_THRESH),
+            },
+        }
 
     def get_likes(self, obj):
-        if obj.likes and obj.likes["users"]:
-            people = []
-            for user in obj.likes["users"]:
-                people.append(UserSerializer(User.objects.get(pk=user)).data)
-            return dict(users=people, user_ids=obj.likes["users"])
+        if obj.likes and obj.likes.get("users"):
+            people = [
+                UserSerializer(User.objects.get(user_id=user)).data
+                for user in obj.likes["users"]
+            ]
+
+            return dict(users=people, user_ids=obj.likes.get("users"))
 
     class Meta:
         model = Posts
         fields = [
             "id",
-            "user_id",
-            "post_text",
-            "post_image",
+            "title",
+            "image",
+            "categories",
+            "title",
             "likes",
-            "created",
-            "updated",
+            "created_at",
+            "updated_at",
+            "height",
+            "width",
+            "author",
+            "src",
         ]
         extra_kwargs = {
-            "created": {"required": False},
-            "updated": {"required": False},
-            "post_image": {"required": False},
+            "created_at": {"required": False},
+            "height": {"required": False},
+            "width": {"required": False},
+            "categories": {"required": False},
+            "src": {"required": False},
+            "updated_at": {"required": False},
             "likes": {"required": False},
         }
 
