97728f4a78220bca4ef0756b9e7b03b8b5bb3810
Anyesh <sthacruz@gmail.com>
2021-06-15 16:24:39 +0545

    chore: Add pagination to post and fix names

--- posts/api/urls.py
+++ posts/api/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-from .views import get_post, get_posts, getLoggedInUserPosts, new_post, userPosts
+from .views import AddNewPost, LoggedInUserPosts, Posts, UserSpecificPosts
 from .views_comments import actionsComment, getPostComments, postNewComment
 
 urlpatterns = [
-    path("user/<int:pk>/posts/", userPosts),
-    path("user/posts/", getLoggedInUserPosts),
-    path("post/new/", new_post),
-    path("post/<int:pk>/", get_post),
-    path("posts/", get_posts),
+    path("user/<int:user_id>/posts/", UserSpecificPosts),
+    path("user/posts/", LoggedInUserPosts),
+    path("post/new/", AddNewPost),
+    path("post/<int:post_id>/", Posts.as_view()),
+    path("post/all/", Posts.as_view()),
     path("<int:post>/comments/", getPostComments),
     path("<int:post_id>/comments/new/", postNewComment),
     path("<int:post_id>/comments/<int:pk>/", actionsComment),
--- posts/api/views.py
+++ posts/api/views.py
@@ -6,28 +6,27 @@ from django.db.models import Q
 from django.http.response import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from accounts.api.serializers import UserSerializer
-from accounts.models import User
 from friends.models import Friend
 from helpers.api_error_response import error_response
 from helpers.error_messages import UNAUTHORIZED
 from notifications.models import Notification
-from posts.api.serializers import PostsSerializer
+from posts.api.serializers import PostCreateSerializer, PostSerializer
 from posts.models import Comment, Post
 from totoro.utils import get_response
 
 
 @api_view(["GET"])
-def userPosts(request, pk):
-    return __userPosts(pk)
+def UserSpecificPosts(request, user_id):
+    return __userPosts(user_id)
 
 
-def __userPosts(pk):
-    user_id = pk
+def __userPosts(user_id):
     data = Post.objects.filter(user_id=user_id)
-    postsSerializer = PostsSerializer(data, many=True)
+    postsSerializer = PostSerializer(data, many=True)
     if postsSerializer.data:
         return Response(data=postsSerializer.data, status=status.HTTP_200_OK)
     else:
@@ -37,7 +36,7 @@ def __userPosts(pk):
 
 
 @api_view(["GET"])
-def getLoggedInUserPosts(request):
+def LoggedInUserPosts(request):
     user_id = request.user.user_id
     # If user_id type is Response that means we have errored
     if type(user_id) is Response:
@@ -45,46 +44,90 @@ def getLoggedInUserPosts(request):
     return __userPosts(user_id)
 
 
-@api_view(["GET"])
-def get_posts(request):
-    user_id = request.user.user_id
-    friends = []
+class PostsPagination(PageNumberPagination):
+    page_size = 50
+    page_size_query_param = "page_size"
 
-    data = Friend.objects.filter(Q(user_a=user_id) | Q(user_b=user_id))
-    if data:
-        friends = [
-            entry.user_a if entry.user_a is not user_id else entry.user_b
-            for entry in data
-        ]
-        friends.append(user_id)
-    else:
-        friends = [user_id]
-    data = Post.objects.filter(author_id__in=friends).order_by("pk").values()
-    posts_final = []
-    for post in data:
-        author = UserSerializer(User.objects.get(pk=post["author_id"])).data
-        posts_final.append(
-            {
-                **PostsSerializer(
-                    Post.objects.get(pk=post["id"]), context={"request": request}
-                ).data,
-                "author": author,
-            }
+    def get_paginated_response(self, data):
+        if not data:
+            return JsonResponse(
+                data=get_response(
+                    message="Post could not be retrieved!",
+                    status_code=404,
+                    status=False,
+                    result={"data": data},
+                ),
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        return JsonResponse(
+            data=get_response(
+                message="Post retrieved succesfully!",
+                status_code=200,
+                status=True,
+                result={
+                    "next": self.get_next_link(),
+                    "previous": self.get_previous_link(),
+                    "count": self.page.paginator.count,
+                    "data": data,
+                },
+            ),
+            status=status.HTTP_200_OK,
         )
 
-    return JsonResponse(
-        data=get_response(
-            message="Post retrieved succesfully!",
-            status_code=200,
-            status=True,
-            result={"data": posts_final},
-        ),
-        status=status.HTTP_200_OK,
-    )
+
+class Posts(APIView):
+    pagination_class = PostsPagination
+    serializer_class = PostSerializer
+
+    @property
+    def paginator(self):
+        if not hasattr(self, "_paginator"):
+            if self.pagination_class is None:
+                self._paginator = None
+            else:
+                self._paginator = self.pagination_class()
+        return self._paginator
+
+    def paginate_queryset(self, queryset):
+        if self.paginator is None:
+            return None
+        return self.paginator.paginate_queryset(queryset, self.request, view=self)
+
+    def get_paginated_response(self, data):
+        assert self.paginator is not None
+        return self.paginator.get_paginated_response(data)
+
+    def get_queryset(self, request, post_id):
+        if post_id:
+            return Post.objects.filter(pk=post_id)
+
+        user_id = request.user.user_id
+        friends = []
+
+        data = Friend.objects.filter(Q(user_a=user_id) | Q(user_b=user_id))
+        if data:
+            friends = [
+                entry.user_a if entry.user_a is not user_id else entry.user_b
+                for entry in data
+            ]
+            friends.append(user_id)
+        else:
+            friends = [user_id]
+        return Post.objects.filter(author_id__in=friends).order_by("pk")
+
+    def get(self, request, post_id=None):
+
+        page = self.paginate_queryset(self.get_queryset(request, post_id))
+        if page is not None:
+            serializer = self.serializer_class(
+                page, many=True, context={"request": request}
+            )
+            return self.get_paginated_response(serializer.data)
 
 
 @api_view(["POST"])
-def new_post(request):
+def AddNewPost(request):
     user = request.user.user_id
 
     req_data = {
@@ -92,19 +135,18 @@ def new_post(request):
         "image": request.data.get("image"),
     }
 
-    postsSerializer = PostsSerializer(
+    serializer = PostCreateSerializer(
         data={
             **req_data,
             **{"author": user},
         },
-        context={"request": request},
     )
-    if postsSerializer.is_valid():
-        postsSerializer.save()
+    if serializer.is_valid():
+        serializer.save()
         return JsonResponse(
             data=get_response(
                 message="Post created succesfully.",
-                result={"data": postsSerializer.data},
+                result={"data": serializer.data},
                 status=True,
                 status_code=201,
             ),
@@ -113,7 +155,7 @@ def new_post(request):
     return JsonResponse(
         data=get_response(
             message="There was an error.",
-            result=postsSerializer.errors,
+            result=serializer.errors,
             status=True,
             status_code=400,
         ),
@@ -121,38 +163,6 @@ def new_post(request):
     )
 
 
-@api_view(["GET"])
-def get_post(request, pk):
-    try:
-        data = Post.objects.get(pk=pk)
-
-        return JsonResponse(
-            data=get_response(
-                message="Post retrieved succesfully!",
-                status_code=200,
-                status=True,
-                result={
-                    "data": PostsSerializer(
-                        Post.objects.get(pk=data.id), context={"request": request}
-                    ).data
-                },
-            ),
-            status=status.HTTP_200_OK,
-        )
-    except Post.DoesNotExist:
-        return JsonResponse(
-            data=get_response(
-                message="Post with given id not found!",
-                status_code=404,
-                status=False,
-                result={"data": []},
-            ),
-            status=status.HTTP_404_NOT_FOUND,
-        )
-
-
-# Like or unlike a post, Auth: REQUIRED
-# -----------------------------------------------
 def likePost(request, post_key):
     user_id = request.user
     # If user_id type is Response that means we have errored
@@ -197,7 +207,7 @@ def editPost(request, post_key):
         post.post_image = request.data["post_image"]
         post.updated = datetime.now(tz=pytz.utc)
         post.save()
-        return Response(PostsSerializer(post).data, status=status.HTTP_200_OK)
+        return Response(PostSerializer(post).data, status=status.HTTP_200_OK)
     else:
         return Response(
             error_response(UNAUTHORIZED), status=status.HTTP_401_UNAUTHORIZED
