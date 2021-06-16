7b71dfed3b1cd4d84b73583cda13d7f850131acb
Anyesh <sthacruz@gmail.com>
2021-06-16 18:17:07 +0545

    fix: Handle outstanding tokens

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from rest_framework_simplejwt.token_blacklist import models
+from rest_framework_simplejwt.token_blacklist.admin import OutstandingTokenAdmin
 
 from accounts.models import Gender, Profile, User
 
+
+class NewOutstandingTokenAdmin(OutstandingTokenAdmin):
+    def has_delete_permission(self, *args, **kwargs):
+        return True
+
+
+admin.site.unregister(models.OutstandingToken)
+admin.site.register(models.OutstandingToken, NewOutstandingTokenAdmin)
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Profile)
