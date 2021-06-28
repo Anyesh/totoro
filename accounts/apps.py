5566d21e15a19fae81724447de1c4670544ec453
Anyesh <sthacruz@gmail.com>
2021-06-28 20:35:41 +0545

    chore: Add signal in app

--- accounts/apps.py
+++ accounts/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class AccountConfig(AppConfig):
     name = "accounts"
+
+    def ready(self):
+        # signals are imported, so that they are defined and can be used
+        import accounts.signals  # noqa
